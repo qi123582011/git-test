dc20a2665ccf78142ed22970c8290a9c1b297e3a
qs <qi123582011@163.com>
2020-07-13 19:41:12 +0800

    把本地工程添加到git

---
+++ day13_git/src/com/sum/git/HelloGitClass.java
@@ -0,0 +1,12 @@
+package com.sum.git;
+
+public class HelloGitClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
